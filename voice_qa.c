8ebae84ae82606313ca68e4d6a991c5e92866314
agh-thu <agh_xjtu@outlook.com>
2021-04-28 20:25:02 +0800

    增加语音质量接口和函数

--- voice_qa.c
+++ voice_qa.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-int voice_qa(const float number)
+int voice_qa(float number)
 {
 	float s_tmp;
 	int Qa_voice = 0;
@@ -10,18 +10,19 @@ int voice_qa(const float number)
 	if (number < 0.0)
 	{
 		printf("the voice quality is wrong \n");
-		return -1;
+		Qa_voice = -1;
 	}
-	else if (0.0< number < 1000.0)
+	else if (number < 1000.0)
 	{
 
 		s_tmp = s_tmp * 8.0 - 0.5;
 		Qa_voice = ceil(s_tmp);
+	
 	}
 	else
 	{
 		printf("pelease check you voice  \n");
-		return 0;
+		Qa_voice = 0;
 	}
 	return Qa_voice;
 }
--- voice_qa.h
+++ voice_qa.h
@@ -1,3 +1,3 @@
 #include<stdio.h>
 #include<math.h>
-int voice_qa(const float number);
+int voice_qa(float number);
